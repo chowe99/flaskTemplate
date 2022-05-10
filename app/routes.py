dab9f4ad557d760a28ef3114e8287ea7ea976c5e
Chowewow <chowewoow@gmail.com>
2022-05-10 13:36:39 +0800

    added hangman files

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template, url_for
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello, world?"
+    return render_template('index.html')
